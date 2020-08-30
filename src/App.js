0d84f76065f118fbe1e1361d90a462a9deae9302
Jones Navela <NavelaJones@gmail.com>
2020-08-30 10:38:04 +0200

    Pass search query

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Header from './components/ui/Header';
-import CharacterGrid from './components/characters/CharacterGrid';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "./components/ui/Header";
+import CharacterGrid from "./components/characters/CharacterGrid";
+import Search from "./components/ui/Search";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
   const [charcters, setCharacters] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState("");
 
   //axios returns a promise
-  useEffect (() => {
+  useEffect(() => {
     const fetchCharacters = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
+      );
 
-      console.log(result.data)
+      // console.log(result.data);
       setCharacters(result.data);
       setIsLoading(false);
-    }
+    };
     fetchCharacters();
-  }, [])//add query for filter
-
+  }, [query]); //add query as a dependency
 
   return (
     <div className="container">
       <Header />
-      <CharacterGrid isLoading={isLoading} characters={charcters}/>
+      <Search getQuery={(q) => setQuery(q)} />
+      <CharacterGrid isLoading={isLoading} characters={charcters} />
     </div>
   );
-}
+};
 
 export default App;
